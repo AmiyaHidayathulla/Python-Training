fb9be1cd5bd91b98fe628a57fbac25ab71b1c81e
AmiyaHidayathulla <amiyahiday208@gmail.com>
2024-07-31 10:57:46 +0530

    another method added

--- rotatedarray.py
+++ rotatedarray.py
@@ -2,3 +2,13 @@ arr=[1,2,3,4,5]
 print("original array: ",arr)
 arr1=arr[2:]+arr[:2]
 print("rotated array: ",arr1)
+
+
+#OR
+def rotate(arr,pos):
+    pos=pos%len(arr)
+    return arr[pos:]+arr[:pos]
+arr=[6,7,8,9,10]
+print(arr)
+pos=2
+print("Rotated array:",rotate(arr,pos))
