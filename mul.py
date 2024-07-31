fb4b253cb8fc66679337554a3fab958d8bec2f06
AmiyaHidayathulla <amiyahiday208@gmail.com>
2024-07-31 12:02:09 +0530

    added

---
+++ mul.py
@@ -0,0 +1,9 @@
+#product of 2 numbers without using * operator
+
+n1=int(input("Enter num 1: "))
+n2=int(input("Enter num 2: "))
+product=0
+for i in range (n1):
+    product=product+n2
+print("Product= ",product)
+
