a3451173431e99b3c527f7a50a3b03f2b88364ac
AmiyaHidayathulla <amiyahiday208@gmail.com>
2024-07-31 14:02:07 +0530

    added

---
+++ sqrt.py
@@ -0,0 +1,10 @@
+import math
+for i in range(1,10,2):
+    s=math.sqrt(i)
+    print(" square root of ",i,"is",s)
+
+#OR
+
+for i in range (1,10,2):
+    s=i**(1/2)
+    print(" square root of ",i,"is",s)
