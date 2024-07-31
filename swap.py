beedfb75dd177bf05c3acbef57349d5f476a7b30
AmiyaHidayathulla <amiyahiday208@gmail.com>
2024-07-31 10:01:35 +0530

    swapping of 2 numbers

---
+++ swap.py
@@ -0,0 +1,22 @@
+x=30
+y=50
+print("x=",x,"y=",y)
+x,y=y,x
+print(" after swapping: x=",x,"y=",y)
+
+x=100
+y=80
+print("x=",x,"y=",y)
+x=x+y
+y=x-y
+x=x-y
+print(" after swapping: x=",x,"y=",y)
+
+
+x=150
+y=120
+print("x=",x,"y=",y)
+x=x^y
+y=x^y
+x=x^y
+print(" after swapping: x=",x,"y=",y)
