71ac1c117902d023e545cc13180d525a35efb350
Amiya Hidayathulla <95171763+AmiyaHidayathulla@users.noreply.github.com>
2025-01-21 21:01:28 +0530

    Create prime_pattern.py

---
+++ prime_pattern.py
@@ -0,0 +1,13 @@
+limit=int(input("Enter the limit: "))
+
+for i in range(2,limit+1):
+          flag=0
+          for j in range(2,i):
+              if(i%j==0):
+                  flag=1
+                  break
+              
+          if(flag==0):
+              print(i)
+          
+          
